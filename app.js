acc34269eaa6271067fa5c958ba8f8f8afa0ad2c
Tomo <tomo.vucic@gmail.com>
2021-01-27 14:53:43 +0100

    support for ejs view engine added and modified view pages accordingly

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const express = require('express');
 // creating an instance of express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>Hrlo Home</p>');
-    res.sendFile('./views/index.html', { root: __dirname});
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    // res.send('<p>Hrlo Home</p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about');
 })
+
+// send 404 page 
+app.use((req, res) => {
+    res.status(404).render('404');
+});
