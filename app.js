2894b7c3de463ee0e773d7349b1953e8575b5894
Tomo <tomo.vucic@gmail.com>
2021-01-28 17:55:39 +0100

    Creating and establishing mongoDB connection through .env file

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // creating an instance of express app
 const app = express();
+require('dotenv').config();
 
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+
+// connect to mongoDB 
+const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.PASSWORD}@cluster0.csvgw.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
+
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then( (result) => { app.listen(3000) })
+        .catch( (err) => { console.log(err)})
 
 // Logging informtions with morgan
 app.use(morgan('dev'));
