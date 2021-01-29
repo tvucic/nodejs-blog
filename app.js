d7d7d0012e40abae600fe9000c0826af997e4ce3
Tomo <tomo.vucic@gmail.com>
2021-01-29 11:11:13 +0100

    Blog model added for database purposes. CRD (without update) opeartions added

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // creating an instance of express app
 const app = express();
+
+// Middleware and static files
+app.use(express.static('public'));
+
+// Post request encoder - for accepting form data
+app.use(express.urlencoded({ extended: true }));
+
+// rewuired for loading .env variables 
 require('dotenv').config();
 
 // register view engine
@@ -21,21 +31,112 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 // Logging informtions with morgan
 app.use(morgan('dev'));
 
-// Middleware and static files
-app.use(express.static('public'));
+
+// mongoose sandbox handlerer
+app.get('/add-blog', (req,res) => {
+
+    const blog = new Blog({
+        title: 'First Blog',
+        snippet: 'Read my second blog',
+        body: 'Hello readers, isn\'t my blog awesomw and again'
+    });
+    blog.save()
+        .then( (result) => {
+            res.send(result)
+        })
+        .catch( (err) => {
+            console.log(err);
+        })
+});
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  app.get('/single-blog', (req, res) => {
+    Blog.findById('600d4c2f0df79715997ddf89')
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', { title : 'Home', blogs});
+    res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { title : 'About'});
+
 })
+
+/*
+
+    // Blog routes
+
+*/
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+      .then(result => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  // Post blog from the form 
+
+  app.post('/blogs', (req, res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+  
+    blog.save()
+      .then(result => {
+        res.redirect('/blogs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  // Get single blog based on _id 
+
+  app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+      .then(result => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+// Delete blog from frontend 
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title : 'Create'});
 })
