5b7fea857d5c8e25267b104c651d583184612161
Tomo <tomo.vucic@gmail.com>
2021-02-03 17:41:31 +0100

    modified app.js. Port variable added from .env file

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.set('view engine', 'ejs');
 
 // connect to mongoDB 
 const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.PASSWORD}@cluster0.csvgw.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
-
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then( (result) => { app.listen(3000) })
-        .catch( (err) => { console.log(err)})
+        .then( () => { app.listen(PORT), () => { console.log(`Server running on port ${PORT}`)} })
+        .catch( (err) => { console.log(err.message)})
 
 // Logging informtions with morgan
 app.use(morgan('dev'));
